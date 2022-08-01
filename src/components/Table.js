474e6e9d22a2160a2fe3c02ff7e671857e6ee746
Luiz Junior <lacjunior129.la@gmail.com>
2022-08-01 14:58:15 -0300

    requisito 7 concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { gastos } = this.props;
+    console.log(gastos.map((gasto) => gasto.value));
     return (
-      <header>
+      <table>
+        <thead>
 
-        <th scope="col">Descrição</th>
-        <th scope="col">Tag</th>
-        <th scope="col">Método de pagamento</th>
-        <th scope="col">Valor</th>
-        <th scope="col">Moeda</th>
-        <th scope="col">Câmbio utilizado</th>
-        <th scope="col">Valor convertido</th>
-        <th scope="col">Moeda de conversão</th>
-        <th scope="col">Editar/Excluir</th>
+          <tr>
+            <th scope="col">Descrição</th>
+            <th scope="col">Tag</th>
+            <th scope="col">Método de pagamento</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Moeda</th>
+            <th scope="col">Câmbio utilizado</th>
+            <th scope="col">Valor convertido</th>
+            <th scope="col">Moeda de conversão</th>
+            <th scope="col">Editar/Excluir</th>
+          </tr>
+        </thead>
 
-      </header>
+        {gastos.map((gasto) => (
+          <tbody key={ gasto.id }>
+
+            <tr>
+              <td>{gasto.description}</td>
+              <td>{gasto.tag}</td>
+              <td>{gasto.method}</td>
+              <td>{Number(gasto.value).toFixed(2)}</td>
+              <td>
+                {Object.values(gasto.exchangeRates)
+                  .filter((exchange) => gasto.currency === exchange.code
+      && exchange.codein !== 'BRLT').map((e) => e.name)}
+              </td>
+              <td>
+                {Object.values(gasto.exchangeRates)
+                  .filter((exchange) => gasto.currency === exchange.code
+      && exchange.codein !== 'BRLT').map((e) => parseFloat(e.ask).toFixed(2))}
+              </td>
+              <td>
+                {(Object.values(gasto.exchangeRates)
+                  .filter((exchange) => gasto.currency === exchange.code
+      && exchange.codein !== 'BRLT')
+                  .map((e) => Number(e.ask)) * Number(gasto.value)).toFixed(2)}
+
+              </td>
+              <td>Real</td>
+              <td><button type="button">Editar/Excluir</button></td>
+            </tr>
+
+          </tbody>
+        ))}
+
+      </table>
     );
   }
 }
+
+Table.propTypes = ({
+  gastos: PropTypes.arrayOf(Object).isRequired,
+});
+
+const mapStateToProps = (state) => ({
+  gastos: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
